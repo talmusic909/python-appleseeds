5c874c1704b9e54fd5b4aae59dbd78e50c94551e
talmusic909 <talmusic909@gmail.com>
2025-04-29 14:20:19 +0300

    nothing has changed

---
+++ try.py
@@ -0,0 +1,7 @@
+sentence=input('put send please')
+length=len(sentence)
+print('the kengthis', length)
+
+color=input('whats ur favorite color')
+age=input('whats ur age')
+animal=('whats ur animals choise')
